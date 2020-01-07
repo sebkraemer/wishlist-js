e6f25a8f88520a3a6d6dd31d7ddae5087ccc9394
Sebastian Krämer <sebkraemer@posteo.org>
2020-01-07 22:44:55 +0100

    initial change: simple webpage that lets you add wishes to a list dynamically
    No saving, no real functionality, only html-js interaction.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,24 @@
-import '../styles/index.scss';
 
-console.log('webpack starterkit');
+var generateWishlistHtml = function(wishlist) {
+    var result = '<ul>';
+    wishlist.forEach(function(wish) {
+        result += `<li>${wish}</li>\n`;
+    });
+    result += '</ul>';
+    return result;
+};
+
+var wishlist = [];
+
+let form = document.getElementById('user-form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let wish = form.elements['wish'];
+    console.log(`wish.value: ${wish.value}`);
+
+    let wishlistHtmlElement = document.getElementById('wishlistText');
+    wishlist.push(wish.value);
+    wishlistHtmlElement.innerHTML = generateWishlistHtml(wishlist);
+});
