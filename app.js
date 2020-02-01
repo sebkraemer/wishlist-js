8e038bc9fc026beb849b09488173528038b3240f
Sebastian Krämer <sebkraemer@posteo.org>
2020-02-01 23:44:08 +0100

    first example route

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
+const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
 
-var port = process.env.PORT || 3000;
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = { hello: "This is my API" };
+
+    res.json(response);
+  });
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my API!');
+  res.send('Welcome to my Nodemon API!');
 });
 
-app.listen(port, ()=> {
-    console.log('Running on port ' + port);
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
 });
