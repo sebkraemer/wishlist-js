16b015122e0a3b4ca7eb1b7d17211e42a6d0b222
Sebastian Krämer <sebkraemer@posteo.org>
2020-02-03 23:14:51 +0100

    WIP: added get and post routes

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI', { useUnifiedTopology: true });
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+
+app.use(bodyParser.urlencoded({ extended: true} ));
+app.use(bodyParser.json());
 
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    console.log(book);
+    return res.json(book);
+  })
   .get((req, res) => {
-    const response = { hello: "This is my API" };
-
-    res.json(response);
+    const query = {};
+    if (req.query.genre) { // only genre filter
+      query.genre = req.query.genre; // todo why does it allow this with const query?
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
+bookRouter.route('/books/:bookId') // will deliver the parameter
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
   });
 app.use('/api', bookRouter);
 
