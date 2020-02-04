e67931944bc1bcdc178a00feacc2d16451c1b2c6
Sebastian Krämer <sebkraemer@posteo.org>
2020-02-04 22:32:34 +0100

    use middleware with 'use()'

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const express = require('express');
 
 function routes(Book) {
@@ -21,14 +22,28 @@ function routes(Book) {
         return res.json(books);
       });
     });
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
   bookRouter.route('/books/:bookId') // will deliver the parameter
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(book);
-      });
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      book.save();
+      return res.json(book);
     });
 
   return bookRouter;
