6b93a801b6e5c4021d8f63d4b709d0d9d1e9c470
Sebastian Krämer <sebkraemer@posteo.org>
2020-02-04 23:13:05 +0100

    implement PATCH and DELETE

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -42,8 +42,43 @@ function routes(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
-      return res.json(book);
+      //req.book.save((err) => {
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req; // necessary??
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+      //book.save(); // same?!
+
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204); // removed
+      });
     });
 
   return bookRouter;
