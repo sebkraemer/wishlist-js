b81ffcdf4a5ba9f7496bf6ca6453cac270931945
Sebastian Krämer <sebkraemer@posteo.org>
2020-02-04 21:09:53 +0100

    add post and refactor into module

--- app.js
+++ app.js
@@ -4,40 +4,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI', { useUnifiedTopology: true });
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-app.use(bodyParser.urlencoded({ extended: true} ));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    console.log(book);
-    return res.json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) { // only genre filter
-      query.genre = req.query.genre; // todo why does it allow this with const query?
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-bookRouter.route('/books/:bookId') // will deliver the parameter
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
